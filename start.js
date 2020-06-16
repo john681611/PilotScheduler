2d3ec7e42006e92bff5adb12e571ec0647897f89
John Harvey <john681611@hotmail.com>
2020-06-16 14:45:30 +0100

    file resort

--- start.js
+++ start.js
@@ -1,2 +1,2 @@
 const port = 3000
-require('./app').listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+require('./src/app').listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
